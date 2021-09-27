381ca65fa264d8519726745f134dbab547d53f0f
jacob-carpenter <jacob.carpenter@casebook.net>
2021-09-27 11:32:37 -0500

    Fixing issue

--- packages/import/tests/schema/import-schema.spec.ts
+++ packages/import/tests/schema/import-schema.spec.ts
@@ -15,90 +15,90 @@ const importSchema = (schema: string, schemas?: Record<string, string>) => {
 const parseSDL = (content: string) => content.split('\n').map(str => str.trim()).filter(str => str.startsWith('# import ') || str.startsWith('#import ')).map(str => parseImportLine(str.replace('#', '').trim()));
 
 describe('importSchema', () => {
-  test.skip('parseImportLine: parse single import', () => {
+  test('parseImportLine: parse single import', () => {
     expect(parseImportLine(`import A from "schema.graphql"`)).toEqual({
       imports: ['A'],
       from: 'schema.graphql',
     });
   });
 
-  test.skip('parseImportLine: optional semicolon', () => {
+  test('parseImportLine: optional semicolon', () => {
     expect(parseImportLine(`import A from "schema.graphql";`)).toEqual({
       imports: ['A'],
       from: 'schema.graphql',
     });
   });
 
-  test.skip('parseImportLine: invalid', () => {
+  test('parseImportLine: invalid', () => {
     expect(() => parseImportLine(`import from "schema.graphql"`)).toThrow();
   });
 
-  test.skip('parseImportLine: invalid 2', () => {
+  test('parseImportLine: invalid 2', () => {
     expect(() => parseImportLine(`import A from ""`)).toThrow();
   });
 
-  test.skip('parseImportLine: invalid 3', () => {
+  test('parseImportLine: invalid 3', () => {
     expect(() => parseImportLine(`import A. from ""`)).toThrow();
   });
 
-  test.skip('parseImportLine: invalid 4', () => {
+  test('parseImportLine: invalid 4', () => {
     expect(() => parseImportLine(`import A.* from ""`)).toThrow();
   });
 
-  test.skip('parseImportLine: parse multi import', () => {
+  test('parseImportLine: parse multi import', () => {
     expect(parseImportLine(`import A, B from "schema.graphql"`)).toEqual({
       imports: ['A', 'B'],
       from: 'schema.graphql',
     });
   });
 
-  test.skip('parseImportLine: parse multi import (weird spacing)', () => {
+  test('parseImportLine: parse multi import (weird spacing)', () => {
     expect(parseImportLine(`import  A  ,B   from "schema.graphql"`)).toEqual({
       imports: ['A', 'B'],
       from: 'schema.graphql',
     });
   });
 
-  test.skip('parseImportLine: different path', () => {
+  test('parseImportLine: different path', () => {
     expect(parseImportLine(`import A from "../new/schema.graphql"`)).toEqual({
       imports: ['A'],
       from: '../new/schema.graphql',
     });
   });
 
-  test.skip('parseImportLine: module in node_modules', () => {
+  test('parseImportLine: module in node_modules', () => {
     expect(parseImportLine(`import A from "module-name"`)).toEqual({
       imports: ['A'],
       from: 'module-name',
     });
   });
 
-  test.skip('parseImportLine: specific field', () => {
+  test('parseImportLine: specific field', () => {
     expect(parseImportLine(`import A.b from "module-name"`)).toEqual({
       imports: ['A.b'],
       from: 'module-name',
     });
   });
 
-  test.skip('parseImportLine: multiple specific fields', () => {
+  test('parseImportLine: multiple specific fields', () => {
     expect(parseImportLine(`import A.b, G.q from "module-name"`)).toEqual({
       imports: ['A.b', 'G.q'],
       from: 'module-name',
     });
   });
 
-  test.skip('parseImportLine: default import', () => {
+  test('parseImportLine: default import', () => {
     expect(parseImportLine(`import "module-name"`)).toEqual({
       imports: ['*'],
       from: 'module-name',
     });
   });
 
-  test.skip('parseSDL: non-import comment', () => {
+  test('parseSDL: non-import comment', () => {
     expect(parseSDL(`#important: comment`)).toEqual([]);
   });
 
-  test.skip('parse: multi line import', () => {
+  test('parse: multi line import', () => {
     const sdl = /* GraphQL */`\
           # import A from 'a.graphql'
           # import * from "b.graphql"
@@ -115,7 +115,7 @@ describe('importSchema', () => {
     ]);
   });
 
-  test.skip('Module in node_modules', () => {
+  test('Module in node_modules', () => {
     const b = `\
           # import lower from './lower.graphql'
           type B {
@@ -149,7 +149,7 @@ describe('importSchema', () => {
     expect(importSchema('./fixtures/import-module/a.graphql')).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('importSchema: imports only', () => {
+  test('importSchema: imports only', () => {
     const expectedSDL = /* GraphQL */`\
           type Query {
             first: String
@@ -160,7 +160,7 @@ describe('importSchema', () => {
     expect(importSchema('./fixtures/imports-only/all.graphql')).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('importSchema: import .gql extension', () => {
+  test('importSchema: import .gql extension', () => {
     const expectedSDL = /* GraphQL */`\
           type A {
             id: ID!
@@ -169,7 +169,7 @@ describe('importSchema', () => {
     expect(importSchema('./fixtures/import-gql/a.gql')).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('importSchema: import duplicate', () => {
+  test('importSchema: import duplicate', () => {
     const expectedSDL = /* GraphQL */`\
           type Query {
             first: String
@@ -180,7 +180,7 @@ describe('importSchema', () => {
     expect(importSchema('./fixtures/import-duplicate/all.graphql')).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('importSchema: import nested', () => {
+  test('importSchema: import nested', () => {
     const expectedSDL = /* GraphQL */`\
           type Query {
             first: String
@@ -191,7 +191,7 @@ describe('importSchema', () => {
     expect(importSchema('./fixtures/import-nested/all.graphql')).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('importSchema: field types', () => {
+  test('importSchema: field types', () => {
     const expectedSDL = /* GraphQL */`\
           type A {
             first: String
@@ -211,7 +211,7 @@ describe('importSchema', () => {
     expect(importSchema('./fixtures/field-types/a.graphql')).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('importSchema: enums', () => {
+  test('importSchema: enums', () => {
     const expectedSDL = /* GraphQL */`\
           type A {
             first: String
@@ -228,7 +228,7 @@ describe('importSchema', () => {
     expect(importSchema('./fixtures/enums/a.graphql')).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('importSchema: import all', () => {
+  test('importSchema: import all', () => {
     const expectedSDL = /* GraphQL */`\
           type A {
             first: String
@@ -253,7 +253,7 @@ describe('importSchema', () => {
     expect(importSchema('./fixtures/import-all/a.graphql')).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('importSchema: import all from objects', () => {
+  test('importSchema: import all from objects', () => {
     const expectedSDL = /* GraphQL */`\
           type A {
             first: String
@@ -278,7 +278,7 @@ describe('importSchema', () => {
     expect(importSchema(`./fixtures/import-all-from-objects/a.graphql`)).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip(`importSchema: single object schema`, () => {
+  test(`importSchema: single object schema`, () => {
     const expectedSDL = /* GraphQL */`\
           type A {
             field: String
@@ -288,7 +288,7 @@ describe('importSchema', () => {
     expect(importSchema('./fixtures/single-object/a.graphql')).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip(`importSchema: import all mix 'n match`, () => {
+  test(`importSchema: import all mix 'n match`, () => {
     const expectedSDL = /* GraphQL */`\
           scalar Date
 
@@ -317,7 +317,7 @@ describe('importSchema', () => {
     expect(importSchema('./fixtures/mix-n-match/a.graphql')).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip(`importSchema: import all mix 'n match 2`, () => {
+  test(`importSchema: import all mix 'n match 2`, () => {
     const expectedSDL = /* GraphQL */`\
           type A {
             first: String
@@ -342,7 +342,7 @@ describe('importSchema', () => {
     expect(importSchema('./fixtures/mix-n-match2/a.graphql')).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip(`importSchema: import all - include Query/Mutation/Subscription type`, () => {
+  test(`importSchema: import all - include Query/Mutation/Subscription type`, () => {
 
     const expectedSDL = /* GraphQL */`\
           type Query {
@@ -372,7 +372,7 @@ describe('importSchema', () => {
     expect(importSchema('./fixtures/include-root-types/a.graphql')).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('importSchema: scalar', () => {
+  test('importSchema: scalar', () => {
     const expectedSDL = /* GraphQL */`\
           type A {
             b: B
@@ -383,7 +383,7 @@ describe('importSchema', () => {
     expect(importSchema('./fixtures/scalar/a.graphql')).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('importSchema: directive', () => {
+  test('importSchema: directive', () => {
     const expectedSDL = /* GraphQL */`\
           type A {
             first: String @upper
@@ -399,7 +399,7 @@ describe('importSchema', () => {
     expect(importSchema('./fixtures/directive/a.graphql')).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('importSchema: key directive', () => {
+  test('importSchema: key directive', () => {
     const expectedSDL = /* GraphQL */`\
           scalar UPC
 
@@ -412,7 +412,7 @@ describe('importSchema', () => {
   });
 
   // TODO: later
-  test.skip('importSchema: multiple key directive', () => {
+  test('importSchema: multiple key directive', () => {
     const expectedSDL = /* GraphQL */`\
           scalar UPC
 
@@ -427,7 +427,7 @@ describe('importSchema', () => {
     expect(importSchema('./fixtures/directive/e.graphql')).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('importSchema: external directive', () => {
+  test('importSchema: external directive', () => {
     const expectedSDL = /* GraphQL */`\
           type Review @key(fields: "id") {
             product: Product @provides(fields: "name")
@@ -441,7 +441,7 @@ describe('importSchema', () => {
     expect(importSchema('./fixtures/directive/f.graphql')).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('importSchema: requires directive', () => {
+  test('importSchema: requires directive', () => {
     const expectedSDL = /* GraphQL */`\
           type Review {
             id: ID
@@ -456,7 +456,7 @@ describe('importSchema', () => {
     expect(importSchema('./fixtures/directive/g.graphql')).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('importSchema: interfaces', () => {
+  test('importSchema: interfaces', () => {
     const expectedSDL = /* GraphQL */`\
           type A implements B {
             first: String
@@ -475,7 +475,7 @@ describe('importSchema', () => {
     expect(importSchema('./fixtures/interfaces/a.graphql')).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('importSchema: interfaces-many', () => {
+  test('importSchema: interfaces-many', () => {
     const expectedSDL = /* GraphQL */`\
           type A implements B {
             first: String
@@ -502,7 +502,7 @@ describe('importSchema', () => {
     expect(importSchema('./fixtures/interfaces-many/a.graphql')).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('importSchema: interfaces-implements', () => {
+  test('importSchema: interfaces-implements', () => {
     const expectedSDL = /* GraphQL */`\
           type A implements B {
             id: ID!
@@ -519,7 +519,7 @@ describe('importSchema', () => {
     expect(importSchema('./fixtures/interfaces-implements/a.graphql')).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('importSchema: interfaces-implements-many', () => {
+  test('importSchema: interfaces-implements-many', () => {
     const expectedSDL = /* GraphQL */`\
           type A implements B {
             id: ID!
@@ -540,7 +540,7 @@ describe('importSchema', () => {
     expect(importSchema('./fixtures/interfaces-implements-many/a.graphql')).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('importSchema: input types', () => {
+  test('importSchema: input types', () => {
     const expectedSDL = /* GraphQL */`\
           type A {
             first(b: B): String
@@ -558,7 +558,7 @@ describe('importSchema', () => {
     expect(importSchema('./fixtures/input-types/a.graphql')).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('importSchema: complex test', () => {
+  test('importSchema: complex test', () => {
     try {
       const a = importSchema('./fixtures/complex/a.graphql');
       expect(a).toBeTruthy();
@@ -567,7 +567,7 @@ describe('importSchema', () => {
     }
   });
 
-  test.skip('circular imports', () => {
+  test('circular imports', () => {
     const expectedSDL = /* GraphQL */`\
           type A {
             first: String
@@ -594,7 +594,7 @@ describe('importSchema', () => {
     expect(actualSDL).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('related types', () => {
+  test('related types', () => {
     const expectedSDL = /* GraphQL */`\
           type A {
             first: String
@@ -615,7 +615,7 @@ describe('importSchema', () => {
     expect(actualSDL).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('relative paths', () => {
+  test('relative paths', () => {
     const expectedSDL = /* GraphQL */`\
           type Query {
             feed: [Post!]!
@@ -641,7 +641,7 @@ describe('importSchema', () => {
     expect(actualSDL).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('root field imports', () => {
+  test('root field imports', () => {
     const expectedSDL = /* GraphQL */`\
           type Query {
             posts(filter: PostFilter): [Post]
@@ -663,7 +663,7 @@ describe('importSchema', () => {
     expect(actualSDL).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('extend root field', () => {
+  test('extend root field', () => {
     const expectedSDL = /* GraphQL */`\
           extend type Query {
             me: User
@@ -678,7 +678,7 @@ describe('importSchema', () => {
     expect(actualSDL).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('extend root field imports', () => {
+  test('extend root field imports', () => {
     const expectedSDL = /* GraphQL */`\
           extend type Query {
             me: User
@@ -698,7 +698,7 @@ describe('importSchema', () => {
     expect(actualSDL).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('merged root field imports', () => {
+  test('merged root field imports', () => {
     const expectedSDL = /* GraphQL */`\
           type Query {
             helloA: String
@@ -723,7 +723,7 @@ describe('importSchema', () => {
     expect(actualSDL).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('global schema modules', () => {
+  test('global schema modules', () => {
     const shared = `
               type Shared {
                 first: String
@@ -743,7 +743,7 @@ describe('importSchema', () => {
     expect(importSchema('./fixtures/global/a.graphql', { shared })).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('missing type on type', () => {
+  test('missing type on type', () => {
     try {
       importSchema('./fixtures/type-not-found/a.graphql');
       throw new Error();
@@ -753,7 +753,7 @@ describe('importSchema', () => {
 
   });
 
-  test.skip('missing type on interface', () => {
+  test('missing type on interface', () => {
     try {
       importSchema('./fixtures/type-not-found/b.graphql');
       throw new Error();
@@ -763,7 +763,7 @@ describe('importSchema', () => {
 
   });
 
-  test.skip('missing type on input type', () => {
+  test('missing type on input type', () => {
     try {
       importSchema('./fixtures/type-not-found/c.graphql');
       throw new Error();
@@ -773,7 +773,7 @@ describe('importSchema', () => {
 
   });
 
-  test.skip('missing interface type', () => {
+  test('missing interface type', () => {
 
     try {
       importSchema('./fixtures/type-not-found/d.graphql');
@@ -784,7 +784,7 @@ describe('importSchema', () => {
 
   });
 
-  test.skip('missing union type', () => {
+  test('missing union type', () => {
 
     try {
       importSchema('./fixtures/type-not-found/e.graphql');
@@ -795,7 +795,7 @@ describe('importSchema', () => {
 
   });
 
-  test.skip('missing type on input type', () => {
+  test('missing type on input type', () => {
 
     try {
       importSchema('./fixtures/type-not-found/f.graphql');
@@ -806,7 +806,7 @@ describe('importSchema', () => {
 
   });
 
-  test.skip('missing type on directive', () => {
+  test('missing type on directive', () => {
 
     try {
       importSchema('./fixtures/type-not-found/g.graphql');
@@ -817,7 +817,7 @@ describe('importSchema', () => {
 
   });
 
-  test.skip('import with collision', () => {
+  test('import with collision', () => {
     // Local type gets preference over imported type
     const expectedSDL = /* GraphQL */`\
           type User {
@@ -829,7 +829,7 @@ describe('importSchema', () => {
     expect(importSchema('./fixtures/collision/a.graphql')).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('merged custom root fields imports', () => {
+  test('merged custom root fields imports', () => {
     const expectedSDL = /* GraphQL */`\
             type Query {
               helloA: String
@@ -851,7 +851,7 @@ describe('importSchema', () => {
     expect(actualSDL).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('respect schema definition', () => {
+  test('respect schema definition', () => {
     const expectedSDL = /* GraphQL */`\
       schema {
         query: MyQuery
@@ -870,7 +870,7 @@ describe('importSchema', () => {
     expect(actualSDL).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('import schema with shadowed type', () => {
+  test('import schema with shadowed type', () => {
     const expectedSDL = /* GraphQL */`\
       type Query {
         b: B!
@@ -883,7 +883,7 @@ describe('importSchema', () => {
     expect(importSchema('./fixtures/import-shadowed/a.graphql')).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('import specific types', () => {
+  test('import specific types', () => {
     const expectedSDL = /* GraphQL */`\
   type User implements B {
     b: String!
@@ -899,7 +899,7 @@ describe('importSchema', () => {
     expect(importSchema('./fixtures/specific/a.graphql')).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('imports missing named imports for file imported multiple time without duplicates', () => {
+  test('imports missing named imports for file imported multiple time without duplicates', () => {
     const expectedSDL = /* GraphQL */`\
   type Query {
     a: B
@@ -918,7 +918,7 @@ describe('importSchema', () => {
     expect(importSchema('fixtures/multiple-imports/schema.graphql')).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('imports multiple imports at least 3 levels deep with transitive dependencies', () => {
+  test('imports multiple imports at least 3 levels deep with transitive dependencies', () => {
     const expectedSDL = /* GraphQL */`
         type Product {
             price: Int
@@ -984,7 +984,7 @@ describe('importSchema', () => {
     expect(importSchema('fixtures/multiple-levels-master-schema/level1.graphql')).toBeSimilarGqlDoc(expectedSDL);
   });
 
-  test.skip('imports multi-level types without direct references', () => {
+  test('imports multi-level types without direct references', () => {
     const expectedSDL = /* GraphQL */`\
   type Level1 {
     id: ID!
